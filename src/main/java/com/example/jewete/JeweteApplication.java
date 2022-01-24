e0a6ac2d44b4431a8e2a65ef31ebdfb4eff4de8e
hendriksutrisna <sutrisnahendrik.gmail.com>
2022-01-24 10:00:54 +0800

    1st commit

---
+++ src/main/java/com/example/jewete/JeweteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jewete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeweteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeweteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jewete/JeweteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jewete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeweteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
